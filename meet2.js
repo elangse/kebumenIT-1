a2ef8a7e864abe3f6fb83420d754b91ef1730ecd
Rom Ady <romady@Roms-MacBook-Pro.local>
2020-01-05 11:18:51 +0700

    #1 mulai buat variabel javascript

---
+++ meet2.js
@@ -0,0 +1,24 @@
+// BASIC JAVASCCRIPT
+// constanta
+    const kelasA = "6";
+    //console.log(kelasA);
+    const kelasB = "7";
+    // console.log(kelasA + kelasB);
+
+// variable
+    var kelasC = 7;
+    console.log(kelasC);
+    kelasC = 10;
+    console.log(kelasC);
+
+// tipe data
+    var nama = "saeful"; //string 
+    var umur = 20; //number
+    var nikah = false; // boolean
+    var dataKosong = null; //null
+    var notFound = undefined; //undefined
+    var dataDiri = { nama: "saeful", umur: 20, nikah: false}
+    console.log(dataDiri);
+
+    var arr = [nama];
+    console.log(arr[0]);
